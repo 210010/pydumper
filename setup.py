6e477bf0bb89deec725e58e40ed7f9f97863b732
Kang Min Yoo <kaniblurous@gmail.com>
2016-04-08 17:31:51 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="pydumper", version="0.2", description="Clean wrapper for cpickle", author="Kang Min Yoo", author_email="kaniblurous@gmail.com", py_modules=['dumper'], url="https://github.com/kaniblu/pydumper")
